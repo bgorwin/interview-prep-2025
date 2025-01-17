6f443f818520921b3f8c0fef75f355254a77e0dd
Brent Gorwin <brent.gorwin@gmail.com>
2025-01-17 15:33:39 -0800

    interview prep before creating branch

--- interview-prep/interview-prep-react/src/App.tsx
+++ interview-prep/interview-prep-react/src/App.tsx
@@ -1,13 +1,28 @@
 import './App.css'
 import './components/ParentComponent';
-import { ParentComponent } from './components/ParentComponent';
+// import { ParentComponent } from './components/ParentComponent';
 // import FilteringQuestionComponent from './components/FilteringQuestionComponent';
+
+function Comment({name, time, children}){
+  return (
+    <section>
+      <p>{name} commented at {time}</p>
+      <p>This is a comment</p>
+      {children}
+    </section>
+  );
+}
 
 function App() {
   return (
     <>
-      <ParentComponent />
+      {/* <ParentComponent /> */}
       {/* <FilteringQuestionComponent /> */}
+
+      <Comment name="Brent" time={(new Date()).toString()}>
+        <p>Hello World</p>
+        <p>This is a comment</p>
+      </Comment>
     </>
   )
 }
